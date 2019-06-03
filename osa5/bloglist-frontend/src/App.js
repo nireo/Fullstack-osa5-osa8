d0e3c522f02db0dcd3622de2f3cd6d878461eabd
nireo <eemil.lehtikangas1@gmail.com>
2019-06-03 15:09:02 +0300

    added notification

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from "./components/Notification"
 
 const App = () => {
 	const [blogs, setBlogs] = useState([])
 	const [username, setUsername] = useState('')
 	const [password, setPassword] = useState('')
+	const [user, setUser] = useState(null)
+	const [errorMessage, setErrorMessage] = useState(null)
 
 	useEffect(() => {
 		blogService.getAll().then(blogs =>
@@ -13,13 +17,24 @@ const App = () => {
 		)  
 	}, [])
 
-	const handleLogin = (event) => {
+	const handleLogin = async (event) => {
 		event.preventDefault()
-		console.log("logging in with", username, password)
+		try {
+			const user = await loginService.login({
+				username, password
+			})
+
+			setUser(user)
+			setUsername('')
+			setPassword('')
+		} catch (exception) {
+			setErrorMessage('käyttäjätunnus tai salasana virheellinen')
+		}
 	}
 	
 	return (
 		<div>
+			<Notification message={errorMessage} />
 			<h2>blogs</h2>
 			<form  onSubmit={handleLogin}>
 				<div>
