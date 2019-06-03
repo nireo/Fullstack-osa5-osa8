4ee9afc7a5c49b93f75f0e1cd1ea31c548cad934
nireo <eemil.lehtikangas1@gmail.com>
2019-06-03 14:09:01 +0300

    added new bloglist-frontend since old not working & added input field

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -3,22 +3,50 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+	const [blogs, setBlogs] = useState([])
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+	useEffect(() => {
+		blogService.getAll().then(blogs =>
+			setBlogs( blogs )
+		)  
+	}, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+	const handleLogin = (event) => {
+		event.preventDefault()
+		console.log("logging in with", username, password)
+	}
+	
+	return (
+		<div>
+			<h2>blogs</h2>
+			<form  onSubmit={handleLogin}>
+				<div>
+					käyttäjätunnus
+					<input 
+						type="text"
+						value={username}
+						name="Username"
+						onChange={({target}) => setUsername(target.value)}
+					/>
+				</div>
+				<div>
+					salasana
+					<input
+						type="password"
+						value={password}
+						name="Password"
+						onChange={({target}) => setPassword(target.value)}
+					/>
+				</div>
+				<button type="submit">kirjaudu</button>
+			</form>
+			{blogs.map(blog =>
+				<Blog key={blog.id} blog={blog} />
+			)}
+		</div>
+	)
 }
 
 export default App
