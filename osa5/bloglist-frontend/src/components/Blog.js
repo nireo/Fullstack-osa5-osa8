d0cc995c52b5ebffa31892e422e6922f9a547fdd
nireo <eemil.lehtikangas1@gmail.com>
2019-06-03 19:46:56 +0300

    added showMore functionality and a bit of styling

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+const Blog = ({ blog }) => {
+    const [showMore, setShowMore] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    if (showMore) {
+        return (
+            <div style={blogStyle} onClick={() => setShowMore(false)}>
+                <div>title: {blog.title}</div>
+                <div>author: {blog.author}</div>
+                <div>url: {blog.url}</div>
+                <div>likes: {blog.likes}</div>
+            </div>
+        )
+    }
+
+    return (
+        <div style={blogStyle} onClick={() => setShowMore(true)}>
+            {blog.title} {blog.author}
+        </div>
+    )
+}
 
 export default Blog
