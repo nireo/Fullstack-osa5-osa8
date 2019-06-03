1762868c5bc0df76deb31fc61aa653133369e41d
nireo <eemil.lehtikangas1@gmail.com>
2019-06-03 15:49:44 +0300

    added button to logout

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -38,11 +38,11 @@ const App = () => {
 	const showBlogs = () => (
 		<div>
 			<h2>{user.name} logged in</h2>
+			<button onClick={() => setUser(null)}>kirjaudu ulos</button>
 			{blogs.map(blog =>
 				<Blog key={blog.id} blog={blog} />
 			)}
 		</div>
-
 	)
 
 	const loginForm = () => (
