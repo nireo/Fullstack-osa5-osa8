60fb167a1e778f3ef27c56eed561b6749cbe2710
nireo <eemil.lehtikangas1@gmail.com>
2019-06-09 11:54:07 +0300

    added test file and installed react-testing-library

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,34 +1,34 @@
 import React, { useState } from 'react'
 
 const Blog = ({ blog, handleLike, handleRemove }) => {
-    const [showMore, setShowMore] = useState(false)
+	const [showMore, setShowMore] = useState(false)
 
-    const blogStyle = {
-        paddingTop: 10,
-        paddingLeft: 2,
-        border: 'solid',
-        borderWidth: 1,
-        marginBottom: 5
-    }
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: 'solid',
+		borderWidth: 1,
+		marginBottom: 5
+	}
 
-    if (showMore) {
-        return (
-            <div style={blogStyle}>
-                <div>title: {blog.title}</div>
-                <div>author: {blog.author}</div>
-                <div>url: {blog.url}</div>
-                <div>likes: {blog.likes}<button onClick={() => handleLike(blog.id)}>like</button>
-                </div>
-                <button onClick={() => setShowMore(false)}>close</button><button onClick={() => handleRemove(blog.id)}>delete</button>
-            </div>
-        )
-    }
+	if (showMore) {
+		return (
+			<div style={blogStyle}>
+				<div>title: {blog.title}</div>
+				<div>author: {blog.author}</div>
+				<div>url: {blog.url}</div>
+				<div>likes: {blog.likes}<button onClick={() => handleLike(blog.id)}>like</button>
+				</div>
+				<button onClick={() => setShowMore(false)}>close</button><button onClick={() => handleRemove(blog.id)}>delete</button>
+			</div>
+		)
+	}
 
-    return (
-        <div style={blogStyle} onClick={() => setShowMore(true)}>
-            {blog.title} {blog.author}
-        </div>
-    )
+	return (
+		<div style={blogStyle} onClick={() => setShowMore(true)}>
+			{blog.title} {blog.author}
+		</div>
+	)
 }
 
 export default Blog
