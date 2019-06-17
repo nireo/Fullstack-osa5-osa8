37f1f8c68abd37720b9b0ada8119591796aeadf2
nireo <eemil.lehtikangas1@gmail.com>
2019-06-17 16:49:26 +0300

    added test for displaying blogs if logged in

--- osa5/bloglist-frontend/src/App.test.js
+++ osa5/bloglist-frontend/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'jest-dom/extend-expect'
 import { render, cleanup, waitForElement } from '@testing-library/react'
+jest.mock('./setupTests.js')
 import App from './App'
 
 afterEach(cleanup)
@@ -18,5 +19,21 @@ describe('App />', () => {
 
 		expect(component.container).toHaveTextContent('login')
 		expect(component.container).not.toHaveTextContent('Blogs:')
+	})
+    
+	test('if user has logged in, display blogs', async () => {
+		const component = render(
+			<App />
+		)
+		const user = {
+			username: 'testi2',
+			token: 'awdaiajwdoij',
+			name: 'Donald Tester'
+		}
+		await localStorage.setItem('loggedBlogUser', JSON.stringify(user))
+
+		component.rerender(<App />)
+        
+		expect(component.container).toHaveTextContent('Blogs:')
 	})
 })
