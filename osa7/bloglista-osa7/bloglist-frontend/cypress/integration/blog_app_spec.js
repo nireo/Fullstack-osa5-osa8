6b91fb793ebafd647d63478a9fcf5deeeb504e19
nireo <eemil.lehtikangas1@gmail.com>
2019-06-23 18:00:07 +0300

    added test for creating blogs

--- osa7/bloglista-osa7/bloglist-frontend/cypress/integration/blog_app_spec.js
+++ osa7/bloglista-osa7/bloglist-frontend/cypress/integration/blog_app_spec.js
@@ -16,4 +16,33 @@ describe('Blog ', function() {
             .click()
         cy.contains('Blogs:')
     })
+
+    describe('when logged in', function() {
+        beforeEach(function() {
+            cy.visit('http://localhost:3000')
+            cy.contains('Login')
+    
+            cy.get('input:first')
+                .type('testi2')
+            cy.get('input:last')
+                .type('salasana')
+            cy.contains('login')
+                .click()
+            cy.contains('Blogs:')
+        })
+
+        it('a blog can be created', function() {
+            cy.contains('new blog')
+                .click()
+            cy.get('#title')
+                .type('Go is a cool language')
+            cy.get('#author')
+                .type('informed developer')
+            cy.get('#url')
+                .type('localhost')
+            cy.contains('add')
+                .click()
+            cy.contains('Go is a cool language')
+        })
+    }) 
 })
--- osa7/bloglista-osa7/bloglist-frontend/src/components/BlogForm.js
+++ osa7/bloglista-osa7/bloglist-frontend/src/components/BlogForm.js
@@ -14,18 +14,21 @@ const BlogForm = ({
 					<h2>create new blog:</h2>
 					<Form.Label>title:</Form.Label>
 					<Form.Control 
+						id="title"
 						value={title.value} 
 						type={title.type}
 						onChange={title.onChange}
 					/>	
 					<Form.Label>author:</Form.Label>
-					<Form.Control
+					<Form.Control 
+						id="author"
 						value={author.value}
 						type={author.type}
 						onChange={author.onChange}
 					/>
 					<Form.Label>url:</Form.Label>
 					<Form.Control 
+						id="url"
 						value={url.value}
 						type={url.type}
 						onChange={url.onChange}
